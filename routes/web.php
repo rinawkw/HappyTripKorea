b1f5f50c72b6f6033ed3189ec6b7118aa34ad660
Rina Wijaya <rinawkw@gmail.com>
2017-05-19 10:45:12 -0700

    hmm

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/blog', function () {
     return view('blog.index');
 });
+//=======
+Route::get('/home/lihatpost', function () {
+    return view('/admin/lihatpost');
+});
+Route::get('/home/tambahpost', function () {
+    return view('/admin/createpost');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+//>>>>>>> cc554d19fff17dd1d7669b4c0028684912407e09
