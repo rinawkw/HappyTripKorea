42f4ac8676ed342ce279f3461cad484572817b70
Winda <winda1996@hotmail.com>
2017-06-03 13:28:35 +0700

    home

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.index')
+
+@section('content')
+	<!-- main content -->
+	<section class="ot-section-b">
+		<!-- container -->
+		<div class="container">
+			<!-- slaider -->
+			<div class="slider">
+				<div class="row row-slider-gutter">
+					<div class="col-md-4 slider-item-small">
+						<figure>
+							<a href="post.html"><img src="images/demo/1200x800-6.jpg" alt=""></a>
+							<figcaption>
+								<h2><a href="post.html">Selfie Fables - Disney Alternative Art Illustrations by Simona Bonafini</a></h2>
+								<div class="post-meta">
+									<span>1. april 2015</span> <span><i class="fa fa-comment-o"></i> 5</span> <span><i class="fa fa-eye"></i> 54</span>
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-md-4 slider-item-small">
+						<figure>
+							<a href="post.html"><img src="images/demo/1200x800-3.jpg" alt=""></a>
+							<figcaption>
+								<h2><a href="post.html">Make Stories Come Alive with Jodi Harvey-Brown Book Sculpture</a></h2>
+								<div class="post-meta">
+									<span>16. april 2015</span> <span><i class="fa fa-comment-o"></i> 3</span> <span><i class="fa fa-eye"></i> 453</span>
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-md-4 slider-item-small">
+						<figure>
+							<a href="post.html"><img src="images/demo/1200x800-5.jpg" alt=""></a>
+							<figcaption>
+								<h2><a href="post.html">Illustrator Turns Your Deepest Fears Into Cute Comics</a></h2>
+								<div class="post-meta">
+									<span>27. april 2015</span> <span><i class="fa fa-comment-o"></i> 23</span> <span><i class="fa fa-eye"></i> 123</span>
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<div class="row row-slider-gutter">
+					<div class="col-md-12 slider-item-large">
+						<figure>
+							<a href="post.html"><img src="images/demo/1200x800-1.jpg" alt=""></a>
+							<figcaption>
+								<h2><a href="post.html">Amazing Game Of Thrones Illustrations By Ivan Belikov</a></h2>
+								<div class="post-meta">
+									<span>27. april 2015</span> <span><i class="fa fa-comment-o"></i> 23</span> <span><i class="fa fa-eye"></i> 123</span>
+								</div>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+			<!-- end slaider -->
+		</div>
+		<!-- end container -->
+	</section>
+	<section class="ot-section-b">
+		<div class="container">
+			<!-- latest posts section -->
+			<h4 class="section-title"><span>The large grid posts</span>Featured Posts</h4>
+			<div class="row">
+				<div class="col-md-4 col-sm-4">
+					<div class="grid-large">
+						<div class="post-image">
+							<div class="review"><span class="score">70</span><span class="percent">%</span></div>
+							<div class="post-title">
+								<div class="gl-text-hover">
+									<p>To set numerous transitions at once, set each individual group of transition values, then use a comma to separate each additional group of transition values.</p>
+									<div class="read-more"><a href="post.html">read more</a></div>
+								</div>
+								<div class="gl-title-hover">
+									<div class="post-cat2"><span>Cinema</span></div>
+									<h2><a href="post.html">Make Stories Come Alive with Jodi Harvey-Brown Book</a></h2>
+								</div>
+							</div>
+							<a href="post.html"><img src="images/demo/720x1080-3.jpg" alt=""></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="grid-large">
+						<div class="post-image">
+							<div class="post-title">
+								<div class="gl-text-hover">
+									<p>To set numerous transitions at once, set each individual group of transition values, then use a comma to separate each additional group of transition values.</p>
+									<div class="read-more"><a href="post.html">read more</a></div>
+								</div>
+								<div class="gl-title-hover">
+									<div class="post-cat2"><span>Cinema</span></div>
+									<h2><a href="post.html">Amazing Game Of Thrones Illustrations By Ivan Belikov</a></h2>
+								</div>
+							</div>
+							<a href="post.html"><img src="images/demo/720x1080-4.jpg" alt=""></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="grid-large">
+						<div class="post-image">
+							<div class="review"><span class="score">70</span><span class="percent">%</span></div>
+							<div class="post-title">
+								<div class="gl-text-hover">
+									<p>To set numerous transitions at once, set each individual group of transition values.</p>
+									<div class="read-more"><a href="post.html">read more</a></div>
+								</div>
+								<div class="gl-title-hover">
+									<div class="post-cat2"><span style="background-color: #DE6069">Graphics</span></div>
+									<h2><a href="post.html">Illustrator Turns Your Deepest Fears Into Cute Comics</a></h2>
+								</div>
+							</div>
+							<a href="post.html"><img src="images/demo/720x1080-2.jpg" alt=""></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- container -->
+	</section>
+@stop
